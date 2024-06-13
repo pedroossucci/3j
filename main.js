167615072a94b3636089a79b22be08a5aa07fa97
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-06-13 08:04:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
+const tempo = document.querySelectorAll(".contador");
+console.log(tempo)
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
