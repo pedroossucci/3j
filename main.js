0f7a8e4a3b4833357b69d2edea3933eae1ea2d04
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-06-14 09:43:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+
+const  tempoObjetivo1 = new Date(2024,11,30);
+const  tempoObjetivo2 = new Date(2024,11,30);
+tempo[0].textContent = tempoObjetivo1
+tempo[1].textContent = tempoObjetivo2
+
+const  agora = new Date()
+
 console.log(tempo)
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
