36a3c9379a2676f3a681055a183e3367344ff22e
pipoket <110671067+pedroossucci@users.noreply.github.com>
2024-07-05 09:47:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,22 +4,35 @@ const tempo = document.querySelectorAll(".contador");
 
 const  tempoObjetivo1 = new Date(2024,11,30);
 const  tempoObjetivo2 = new Date(2024,11,30);
-tempo[0].textContent = tempoObjetivo1
-tempo[1].textContent = tempoObjetivo2
 
-const  agora = new Date()
-let segundos;
-let minutos;
-let horas;
-let dias;
-segundos=(tempoObjetivo1-agora)/1000
-minutos=segundos/60
-horas=minutos/60
-dias=horas/24
-tempo[0].textContent=tempoObjetivo1-agora
-tempo[0].textContent=tempoObjetivo2-agora
-console.log(tempo)
-for(let i = 0; 1 < botoes.length ; i++){
+function calculaTempo(tempoObjetivo){
+    const agora = new Date()
+    let segundos
+    let minutos
+    let horas
+    let dia
+    segundos = (tempoObjetivo-agora)/1000
+    minutos = segundos/60
+    horas = minutos/60
+    dias = horas/24
+    segundos = Math.floor(segundos);
+    minutos = Math.floor(minutos);
+    horas = Math.floor(horas)
+    dias = Math.floor(dias)
+
+    segundos = segundos%60
+    minutos = minutos%60
+    horas = horas%24
+
+    return `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+
+}
+
+  tempo[0].textContent = calculaTempo (tempoObjetivo1);
+  tempo[1].textContent = calculaTempo (tempoObjetivo2); 
+
+
+for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
